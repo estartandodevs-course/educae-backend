d34442d8c068b60f3d1127be00d029eee0fea6c9
brenda <bbritob8@gmail.com>
2024-12-10 17:54:45 -0300

    incluindo formatação de data e atividade id

--- src/comunicacao/educae.comunicacao.app/ViewModels/AtividadeViewModel.cs
+++ src/comunicacao/educae.comunicacao.app/ViewModels/AtividadeViewModel.cs
@@ -4,18 +4,21 @@ namespace educae.comunicacao.app.ViewModels;
 
 public class AtividadeViewModel
 {
+    public Guid AtividadeId { get; set; }
     public string Titulo { get; set; }
     public string Descricao { get; set; }
-    public DateTime DataMaximaEntrega { get; set; }
+    public string DataMaximaEntrega { get; set; }
     public int AvaliacaoDaExecucao { get; set; }
     public bool Feito { get; set; }
+    
     public static AtividadeViewModel Mapear ( Atividade atividade )
     { 
         return new AtividadeViewModel()
         {
+            AtividadeId = atividade.Id,
             Titulo = atividade.Titulo,
             Descricao = atividade.Descricao,
-            DataMaximaEntrega = atividade.DataMaximaEntrega,
+            DataMaximaEntrega = atividade.DataMaximaEntrega.ToString("dd/MM/yyyy"),
             AvaliacaoDaExecucao = atividade.AvaliacaoDaExecucao,
             Feito = atividade.Feito
         };
