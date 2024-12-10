c8a7e63d0e13cb90e2a50423fddf45c38fc4cdb9
brenda <bbritob8@gmail.com>
2024-12-10 17:02:03 -0300

    incluindo error message

--- src/comunicacao/educae.comunicacao.app/Models/SolicitacaoFeedBackModel.cs
+++ src/comunicacao/educae.comunicacao.app/Models/SolicitacaoFeedBackModel.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace educae.comunicacao.app.Models;
 
 public class SolicitacaoFeedBackModel
 {
+    [Required(ErrorMessage = "A propriedade {0} é obrigatória")]
     public string Assunto { get; set; }
+    [Required(ErrorMessage = "A propriedade {0} é obrigatória")]
     public string Conteudo { get; set; }
+    [Required(ErrorMessage = "A propriedade {0} é obrigatória")]
     public UsuarioModel EducadorDestinatario { get; set; }
     public UsuarioModel? AlunoRementente { get; set; }
     public bool EnvioAnonimo { get; set; }
@@ -13,7 +18,9 @@ public class SolicitacaoFeedBackModel
 
 public class UsuarioModel
 {
+    [Required(ErrorMessage = "A propriedade {0} é obrigatória")]
     public string Nome { get; set; }
+    [Required(ErrorMessage = "A propriedade {0} é obrigatória")]
     public string Email { get; set; }
 }
 
