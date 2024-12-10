4eaa0924002a4dadfd0f939adbf8ad2a4d78c93b
brenda <bbritob8@gmail.com>
2024-12-10 14:20:08 -0300

    incluindo construtor

--- src/comunicacao/educae.comunicacao.domain/ValueObject/Usuario.cs
+++ src/comunicacao/educae.comunicacao.domain/ValueObject/Usuario.cs
@@ -5,4 +5,13 @@ public class Usuario
     public Guid Id { get; private set; }
     public string Nome { get; private set; }
     public string Email { get; private set; }
+    
+    protected Usuario() {}
+
+    public Usuario(Guid id, string nome, string email)
+    {
+        Id = id;
+        Nome = nome;
+        Email = email;
+    }
 }
