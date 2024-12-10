3f36818d36bc5ec6d515d1b41fb6187d86b1ecf1
brenda <bbritob8@gmail.com>
2024-12-10 17:55:50 -0300

    incluindo formatação de data e comunicado id

--- src/comunicacao/educae.comunicacao.app/ViewModels/ComunicadoViewModel.cs
+++ src/comunicacao/educae.comunicacao.app/ViewModels/ComunicadoViewModel.cs
@@ -4,18 +4,20 @@ using EstartandoDevsCore.ValueObjects;
 namespace educae.comunicacao.app.ViewModels;
 public class ComunicadoViewModel
 {
+    public Guid ComunicadoId { get; set; }
     public string Titulo { get; set; }
     public string Descricao { get; set; }
     public string Imagem { get; set; }
-    public DateTime DataExpiracao { get; set; }
+    public string DataExpiracao { get; set; }
     public static ComunicadoViewModel Mapear (Comunicado comunicado)
     {
         return new ComunicadoViewModel ()
         {
+            ComunicadoId = comunicado.Id,
             Titulo = comunicado.Titulo,
             Descricao = comunicado.Descricao,
             Imagem = comunicado.Imagem,
-            DataExpiracao = comunicado.DataExpiracao
+            DataExpiracao = comunicado.DataExpiracao.ToString("dd/MM/yyyy")
         };
     }
 }
