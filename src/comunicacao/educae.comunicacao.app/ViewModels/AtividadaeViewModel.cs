3be9fb9a257ac40980ff697feff31df8437c0528
Erickssom Melgaco de Oliveira <erickssommelgaco@gmail.com>
2024-12-10 16:10:08 -0300

    Comunicado ViewModel

---
+++ src/comunicacao/educae.comunicacao.app/ViewModels/AtividadaeViewModel.cs
@@ -0,0 +1,22 @@
+namespace educae.comunicacao.app.ViewModels;
+
+public class AtvidadeViewModel
+{
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DataMaximaEntrega { get; set; }
+        public int AvaliacaoDaExecucao { get; set; }
+        public bool Feito { get; set; }
+            public static AtividadeViewModel Mapear (Atividade atividade)
+            { 
+            return new AtividadeViewModel()
+                {
+                    Titulo = atividade.titulo,
+                    Descricao = atividade.descricao,
+                    DataMaximaEntrega = atividade.dataMaximaEntrega,
+                    AvaliacaoDaExecucao = atividade.avaliacaoDaExecucao,
+                    Feito = atividade.feito
+                };
+            }
+}
+
