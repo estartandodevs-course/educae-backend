c780988f88e645b9c2ef196b8943c20f3a69568f
Erickssom Melgaco de Oliveira <erickssommelgaco@gmail.com>
2024-12-10 17:23:31 -0300

    ViewModel de Comunicacao (para mim esta como um erro mas esta certo)

--- src/comunicacao/educae.comunicacao.app/ViewModels/AtividadeViewModel.cs
+++ src/comunicacao/educae.comunicacao.app/ViewModels/AtividadeViewModel.cs
@@ -1,13 +1,13 @@
 namespace educae.comunicacao.app.ViewModels;
 
-public class AtvidadeViewModel
+public class AtividadeViewModel
 {
         public string Titulo { get; set; }
         public string Descricao { get; set; }
         public DateTime DataMaximaEntrega { get; set; }
         public int AvaliacaoDaExecucao { get; set; }
         public bool Feito { get; set; }
-            public static AtividadeViewModel Mapear (Atividade atividade)
+            public static AtividadeViewModel Mapear ( Atividade atividade )
             { 
                 return new AtividadeViewModel()
                 {
