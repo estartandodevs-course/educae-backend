f040792a6fdba3644262ade63a25196a73431125
brenda <bbritob8@gmail.com>
2024-12-10 18:16:49 -0300

    correção solicitação feed back view model

--- src/comunicacao/educae.comunicacao.app/ViewModels/SolicitacaoFeedback.cs
+++ src/comunicacao/educae.comunicacao.app/ViewModels/SolicitacaoFeedback.cs
@@ -1,4 +1,5 @@
 using educae.comunicacao.domain.Entities;
+using educae.comunicacao.domain.ValueObject;
 
 namespace educae.comunicacao.app.ViewModels;
 
@@ -7,22 +8,33 @@ public class SolicitacaoFeedbackViewModel
 {
     public string Assunto { get; set; }
     public string Conteudo { get; set; }
-    public Usuario EducadorDestinatario { get; set; }
-    public Usuario? AlunoRementente { get; set; }
+    public Guid EducadorDestinatarioId { get; set; }
+    public string EducadorNome { get; set; }
+    public string EducadorEmail { get; set; }
+    public Guid? AlunoRemetenteId { get; set; }
+    public string? AlunoNome { get; set; }
+    public string? AlunoEmail { get; set; }
     public bool EnvioAnonimo { get; set; }
     public bool Aberta { get; set; }
-    public RespostaFeedBack? Resposta { get; set; }
+    public string Resposta { get; set; }
+    public string DataResposta { get; set; }
 
     public SolicitacaoFeedbackViewModel Mapear (SolicitacaoFeedback solicitacaoFeedback)
     {
         return new SolicitacaoFeedbackViewModel () 
         {
-        Assunto = solicitacaoFeedback.assunto,
-        Conteudo = solicitacaoFeedback.conteudo,
-        EducadorDestinatario = solicitacaoFeedback.educadorDestinatario,
-        AlunoRementente = solicitacaoFeedback.alunoRementente,
-        EnvioAnonimo = solicitacaoFeedback.envioAnonimo,
-        Aberta = solicitacaoFeedback.aberta
+            Assunto = solicitacaoFeedback.Assunto,
+            Conteudo = solicitacaoFeedback.Conteudo,
+            EducadorDestinatarioId = solicitacaoFeedback.EducadorDestinatario.Id,
+            EducadorNome = solicitacaoFeedback.EducadorDestinatario.Nome,
+            EducadorEmail = solicitacaoFeedback.EducadorDestinatario.Email,
+            AlunoRemetenteId = solicitacaoFeedback?.AlunoRementente.Id,
+            AlunoNome = solicitacaoFeedback?.AlunoRementente.Nome,
+            AlunoEmail = solicitacaoFeedback?.AlunoRementente.Email,
+            EnvioAnonimo = solicitacaoFeedback.EnvioAnonimo,
+            Aberta = solicitacaoFeedback.Aberta,
+            Resposta = solicitacaoFeedback.Resposta.Resposta,
+            DataResposta = solicitacaoFeedback.Resposta.DataResposta.ToString()        
         };
     }
 }
