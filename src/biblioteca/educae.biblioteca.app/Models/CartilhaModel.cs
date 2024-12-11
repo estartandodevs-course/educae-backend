de6c5c1665ddd8aacb5de16ae5c7fb7376e693fc
brenda <bbritob8@gmail.com>
2024-12-10 22:44:40 -0300

    incluindo validações

--- src/biblioteca/educae.biblioteca.app/Models/CartilhaModel.cs
+++ src/biblioteca/educae.biblioteca.app/Models/CartilhaModel.cs
@@ -1,11 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace educae.biblioteca.app.Models;
 
 public class CartilhaModel
 {
+    [Required(ErrorMessage = "A propriedade {0} é obrigatória")]
     public string Titulo { get; set; }
+    [Required(ErrorMessage = "A propriedade {0} é obrigatória")]
     public string Resumo { get; set; }
+    [Required(ErrorMessage = "A propriedade {0} é obrigatória")]
     public string Descricao { get; set; }
     public string Url {get; set;}
+    [Required(ErrorMessage = "A propriedade {0} é obrigatória")]
     public string Autor { get; set; }
     public int QuantidadeDeAnexos { get; set; }
     public List<AnexoModel> Anexos {get; set;}
