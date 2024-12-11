16417c8f9f94a3220f4e6d7a353717ce72eaf429
brenda <bbritob8@gmail.com>
2024-12-10 23:53:08 -0300

    incluindo construtores para anexo

--- src/biblioteca/educae.biblioteca.domain/ValueObject/Anexo.cs
+++ src/biblioteca/educae.biblioteca.domain/ValueObject/Anexo.cs
@@ -4,4 +4,12 @@ public class Anexo
 {
     public string NomeArquivo { get; set; }
     public string NomeOriginal { get; set; }
+
+    protected Anexo(){}
+
+    public Anexo(string nomeArquivo, string nomeOriginal)
+    {
+        NomeArquivo = nomeArquivo;
+        NomeOriginal = nomeOriginal;
+    }
 }
