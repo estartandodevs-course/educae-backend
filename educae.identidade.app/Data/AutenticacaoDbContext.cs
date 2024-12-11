90643c2af78c4dc480a71b3f3d73c36311b1e161
brenda <bbritob8@gmail.com>
2024-12-11 15:29:51 -0300

    incluindo contexto de autenticação

---
+++ educae.identidade.app/Data/AutenticacaoDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace educae.identidade.App.Data;
+
+public class AutenticacaoDbContext : IdentityDbContext
+{
+    public AutenticacaoDbContext(DbContextOptions<AutenticacaoDbContext> options) : base(options) { }
+}
