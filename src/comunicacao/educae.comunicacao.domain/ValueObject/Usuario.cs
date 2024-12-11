b61178d096a7ef82693009b88d9515692669b94d
brenda <bbritob8@gmail.com>
2024-12-11 13:52:24 -0300

    incluindo campo foto

--- src/comunicacao/educae.comunicacao.domain/ValueObject/Usuario.cs
+++ src/comunicacao/educae.comunicacao.domain/ValueObject/Usuario.cs
@@ -5,13 +5,15 @@ public class Usuario
     public Guid Id { get; private set; }
     public string Nome { get; private set; }
     public string Email { get; private set; }
+    public string Foto { get; private set; }
     
     protected Usuario() {}
 
-    public Usuario(Guid id, string nome, string email)
+    public Usuario(Guid id, string nome, string email, string foto)
     {
         Id = id;
         Nome = nome;
         Email = email;
+        Foto = foto;
     }
 }
