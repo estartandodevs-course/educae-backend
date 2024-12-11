eb40b8cbd36d0ce7027e8a1cef1d60aad79cec8b
brenda <bbritob8@gmail.com>
2024-12-10 21:09:04 -0300

    incluindo contexto biblioteca

---
+++ src/biblioteca/educae.biblioteca.infra/Data/BibliotecaContext.cs
@@ -0,0 +1,84 @@
+using educae.biblioteca.domain.Entities;
+using educae.biblioteca.domain.ValueObject;
+using EstartandoDevsCore.Data;
+using EstartandoDevsCore.DomainObjects;
+using EstartandoDevsCore.Mediator;
+using EstartandoDevsCore.Messages;
+using EstartandoDevsCore.Ultilities;
+using FluentValidation.Results;
+using Microsoft.EntityFrameworkCore;
+
+namespace educae.biblioteca.infra.Data;
+
+public class ComunicacaoContext : DbContext, IUnitOfWorks
+{
+    private readonly IMediatorHandler _mediatorHandler;
+
+    public DbSet<Cartilha> Cartilhas { get; set; }
+    public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<Anexo> Anexos { get; set; }
+
+
+    public ComunicacaoContext(DbContextOptions<ComunicacaoContext> options, IMediatorHandler mediatorHandler)
+        : base(options)
+    {
+        _mediatorHandler = mediatorHandler;
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Ignore<ValidationResult>();
+        modelBuilder.Ignore<Event>();
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ComunicacaoContext).Assembly);
+    }
+
+    public async Task<bool> Commit()
+    {
+        var cetZone = ZonaDeTempo.ObterZonaDeTempo();
+
+        foreach (var entry in ChangeTracker.Entries()
+                     .Where(entry => entry.Entity.GetType().GetProperty("DataDeCadastro") != null))
+        {
+            if (entry.State == EntityState.Added)
+            {
+                entry.Property("DataDeCadastro").CurrentValue =
+                    TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, cetZone);
+            }
+
+            if (entry.State == EntityState.Modified)
+            {
+                entry.Property("DataDeCadastro").IsModified = false;
+                entry.Property("DataDeAlteracao").CurrentValue =
+                    TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, cetZone);
+            }
+        }
+
+        var sucesso = await SaveChangesAsync() > 0;
+
+        if (sucesso) await _mediatorHandler.PublicarEventos(this);
+
+        return sucesso;
+    }
+}
+
+public static class MediatorExtension
+    {
+        public static async Task PublicarEventos<T>(this IMediatorHandler mediator, T ctx) where T : DbContext
+        {
+            var domainEntities = ctx.ChangeTracker
+                .Entries<Entity>()
+                .Where(x => x.Entity.Notificacoes != null && x.Entity.Notificacoes.Any());
+
+            var domainEvents = domainEntities
+                .SelectMany(x => x.Entity.Notificacoes)
+                .ToList();
+
+            domainEntities.ToList()
+                .ForEach(entity => entity.Entity.LimparEventos());
+
+            var tasks = domainEvents.Select(async (domainEvent) => { await mediator.PublicarEvento(domainEvent); });
+
+            await Task.WhenAll(tasks);
+        }
+    }
