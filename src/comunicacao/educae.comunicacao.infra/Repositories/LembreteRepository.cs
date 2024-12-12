20f66a97c75ab97541efc18134ab908f61b7ccfd
Lucas Lima <lucasbezerra14.ll@gmail.com>
2024-12-12 17:39:44 -0300

    feat: Implementação das repositories

--- src/comunicacao/educae.comunicacao.infra/Repositories/LembreteRepository.cs
+++ src/comunicacao/educae.comunicacao.infra/Repositories/LembreteRepository.cs
@@ -1,6 +1,62 @@
+using educae.comunicacao.domain.Entities;
+using educae.comunicacao.domain.Interfaces;
+using educae.comunicacao.infra.Data;
+using EstartandoDevsCore.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace educae.comunicacao.infra.Repositories;
 
-public class LembreteRepository
+public class LembreteRepository : ILembreteRepository
 {
-    
+  private readonly ComunicacaoContext _context;
+
+  public LembreteRepository(ComunicacaoContext context)
+  {
+    _context = context;
+  }
+
+  public IUnitOfWorks UnitOfWork => _context;
+
+  public async Task<Lembrete> ObterPorId(Guid Id)
+  {
+    return await _context.Lembretes.FirstOrDefaultAsync(x => x.Id == Id);
+  }
+
+  public void Adicionar(Lembrete entity)
+  {
+    _context.Lembretes.Add(entity);
+  }
+
+  public void Atualizar(Lembrete entity)
+  {
+    _context.Lembretes.Update(entity);
+  }
+
+  public void Apagar(Func<Lembrete, bool> predicate)
+  {
+    var lembrete = _context.Lembretes.FirstOrDefault(predicate);
+    if (lembrete != null)
+    {
+      _context.Lembretes.Remove(lembrete);
+    }
+  }
+
+  public async Task<IEnumerable<Lembrete>> ObterLembretesPendentes()
+  {
+    return await _context.Lembretes
+        .Where(x => !x.Concluido)
+        .ToListAsync();
+  }
+
+  public async Task<IEnumerable<Lembrete>> ObterLembretesFinalizados()
+  {
+    return await _context.Lembretes
+        .Where(x => x.Concluido)
+        .ToListAsync();
+  }
+
+  public void Dispose()
+  {
+    _context?.Dispose();
+  }
 }
--- src/comunicacao/educae.comunicacao.infra/Repositories/SolicitacaoRepository.cs
+++ src/comunicacao/educae.comunicacao.infra/Repositories/SolicitacaoRepository.cs
@@ -1,6 +1,64 @@
+using educae.comunicacao.domain.Entities;
+using educae.comunicacao.domain.Interfaces;
+using educae.comunicacao.infra.Data;
+using EstartandoDevsCore.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace educae.comunicacao.infra.Repositories;
 
-public class SolicitacaoRepository
+public class SolicitacaoRepository : ISolicitacaoFeedBackRepository
 {
-    
+  private readonly ComunicacaoContext _context;
+
+  public SolicitacaoRepository(ComunicacaoContext context)
+  {
+    _context = context;
+  }
+
+  public IUnitOfWorks UnitOfWork => _context;
+
+  public async Task<SolicitacaoFeedback> ObterPorId(Guid Id)
+  {
+    return await _context.Solicitacoes.FirstOrDefaultAsync(x => x.Id == Id);
+  }
+
+  public void Adicionar(SolicitacaoFeedback entity)
+  {
+    _context.Solicitacoes.Add(entity);
+  }
+
+  public void Atualizar(SolicitacaoFeedback entity)
+  {
+    _context.Solicitacoes.Update(entity);
+  }
+
+  public void Apagar(Func<SolicitacaoFeedback, bool> predicate)
+  {
+    var solicitacao = _context.Solicitacoes.FirstOrDefault(predicate);
+    if (solicitacao != null)
+    {
+      _context.Solicitacoes.Remove(solicitacao);
+    }
+  }
+
+  public async Task<IEnumerable<SolicitacaoFeedback>> ObterSolicitacoesEmAberto()
+  {
+    return await _context.Solicitacoes
+        .Where(x => x.Aberta)
+        .OrderBy(x => x.DataDeCadastro)
+        .ToListAsync();
+  }
+
+  public async Task<IEnumerable<SolicitacaoFeedback>> ObterSolicitacoesFechadas()
+  {
+    return await _context.Solicitacoes
+        .Where(x => !x.Aberta)
+        .OrderBy(x => x.DataDeCadastro)
+        .ToListAsync();
+  }
+
+  public void Dispose()
+  {
+    _context?.Dispose();
+  }
 }
