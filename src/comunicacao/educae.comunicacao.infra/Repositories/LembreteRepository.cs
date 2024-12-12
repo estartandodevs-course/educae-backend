18d31952bb94204d517a282fcd1730d4a175148b
Lucas Lima <lucasbezerra14.ll@gmail.com>
2024-12-12 19:29:11 -0300

    fix: Ajustes nas repositories

--- src/comunicacao/educae.comunicacao.infra/Repositories/LembreteRepository.cs
+++ src/comunicacao/educae.comunicacao.infra/Repositories/LembreteRepository.cs
@@ -1,44 +1,65 @@
 using educae.comunicacao.domain.Entities;
 using educae.comunicacao.domain.Interfaces;
+using educae.comunicacao.infra.Data;
 using EstartandoDevsCore.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace educae.comunicacao.infra.Repositories;
 
 public class LembreteRepository : ILembreteRepository
 {
-    public void Dispose()
+    private readonly ComunicacaoContext _context;
+
+    public LembreteRepository(ComunicacaoContext context)
     {
-        throw new NotImplementedException();
+        _context = context;
     }
 
-    public Task<Lembrete> ObterPorId(Guid Id)
+    public IUnitOfWorks UnitOfWork => _context;
+
+    public async Task<Lembrete> ObterPorId(Guid id)
     {
-        throw new NotImplementedException();
+        return await _context.Lembretes
+            .FirstOrDefaultAsync(x => x.Id == id);
     }
 
     public void Adicionar(Lembrete entity)
     {
-        throw new NotImplementedException();
+        _context.Lembretes.Add(entity);
     }
 
     public void Atualizar(Lembrete entity)
     {
-        throw new NotImplementedException();
+        _context.Lembretes.Update(entity);
     }
 
     public void Apagar(Func<Lembrete, bool> predicate)
     {
-        throw new NotImplementedException();
+        var lembrete = _context.Lembretes.FirstOrDefault(predicate);
+        if (lembrete != null)
+        {
+            _context.Lembretes.Remove(lembrete);
+        }
     }
 
-    public IUnitOfWorks UnitOfWork { get; }
-    public Task<IEnumerable<Lembrete>> ObterLembretesPendentes()
+    public async Task<IEnumerable<Lembrete>> ObterLembretesPendentes()
     {
-        throw new NotImplementedException();
+        return await _context.Lembretes
+            .Where(x => !x.Concluido)
+            .OrderByDescending(x => x.DataDeCadastro)
+            .ToListAsync();
     }
 
-    public Task<IEnumerable<Lembrete>> ObterLembretesFinalizados()
+    public async Task<IEnumerable<Lembrete>> ObterLembretesFinalizados()
     {
-        throw new NotImplementedException();
+        return await _context.Lembretes
+            .Where(x => x.Concluido)
+            .OrderByDescending(x => x.DataDeCadastro)
+            .ToListAsync();
+    }
+
+    public void Dispose()
+    {
+        _context?.Dispose();
     }
 }
