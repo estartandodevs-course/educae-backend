5b1ab1753ecce827bc4bd300063a918058fcf440
Lucas Lima <lucasbezerra14.ll@gmail.com>
2024-12-12 12:46:32 -0300

    feat: Ajuste nas interfaces das queries

--- src/comunicacao/educae.comunicacao.app/Application/Queries/interfaces/IAtividadeQuery.cs
+++ src/comunicacao/educae.comunicacao.app/Application/Queries/interfaces/IAtividadeQuery.cs
@@ -5,4 +5,7 @@ namespace educae.comunicacao.app.Application.Queries.Interfaces;
 public interface IAtividadeQuery : IDisposable
 {
     Task<IEnumerable<AtividadeViewModel>> ObterAtividades();
+    Task<IEnumerable<AtividadeViewModel>> ObterAtividadesFeitas();
+    Task<IEnumerable<AtividadeViewModel>> ObterAtividadesPendentes();
+    Task<IEnumerable<AtividadeViewModel>> ObterAtividadesAtrasadas();
 }
